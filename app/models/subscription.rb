01a48e52af510e979b5ea04b6de731f076961e89
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-04-20 12:09:33 -0400

    Add relationships and validations

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,2 +1,8 @@
 class Subscription < ApplicationRecord
+  belongs_to :customer
+  has_many :tea_subscriptions
+  has_many :teas, through: :tea_subscriptions
+  validates_presence_of :title
+  validates_presence_of :price
+  validates_presence_of :status
 end
