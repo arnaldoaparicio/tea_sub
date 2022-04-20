3c13694126f542713fbba6f29faa1680922ee27d
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-04-20 12:00:57 -0400

    Initial commit

---
+++ db/migrate/20220420155818_create_subscription.rb
@@ -0,0 +1,9 @@
+class CreateSubscription < ActiveRecord::Migration[5.2]
+  def change
+    create_table :subscriptions do |t|
+      t.string :title
+      t.integer :price
+      t.boolean :status
+    end
+  end
+end
