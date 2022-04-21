4eb943fd16ab0a832e6efdd458f38c116b8e9a2b
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-04-21 15:24:44 -0400

    Adjust validation

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,5 +4,5 @@ class Subscription < ApplicationRecord
   has_many :teas, through: :tea_subscriptions
   validates_presence_of :title
   validates_presence_of :price
-  validates_presence_of :status
+  validates_presence_of :status, allow_blank: true
 end
