bfdfc0841e9513d81019122c04ae2b5160329a17
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-04-21 22:53:07 -0400

    Add update and create methods

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::SubscriptionsController < ApplicationController
   def index
     if params.has_key?(:customer_id)
       customer = Customer.find(params[:customer_id])
-      render json: (SubscriptionSerializer.new(customer.subscriptions))
+      render json: SubscriptionSerializer.new(customer.subscriptions)
     else
       render json: SubscriptionSerializer.new(Subscription.all)
     end
@@ -11,8 +11,8 @@ class Api::V1::SubscriptionsController < ApplicationController
   def update
     subscription = Subscription.find(params[:id])
     if subscription.update(subscription_params)
-      tea = subscription.teas.first
-      TeaSubscription.find_by(tea: tea, subscription: subscription).delete
+      # tea = subscription.teas.first
+      # TeaSubscription.find_by(tea: tea, subscription: subscription).delete
       render json: SubscriptionSerializer.new(subscription)
     else
       render status: 400
@@ -20,6 +20,7 @@ class Api::V1::SubscriptionsController < ApplicationController
   end
 
   def create
+    binding.pry
     subscription = Subscription.new(subscription_params)
     tea = Tea.find(params[:tea])
     if subscription.save
