08aec42083a1da24ae60a963e1aef97a8c098246
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-04-22 06:46:55 -0400

    Modify seeds to include frequency to subscriptions

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,8 @@
 customer1 = Customer.create!(first_name: 'Jon', last_name: 'Doe', email: 'jon@mail.com', address: '12 Main St.')
 customer2 = Customer.create!(first_name: 'Jane', last_name: 'Doe', email: 'jane@mail.com', address: '21 6th St.')
 
-subscription1 = customer1.subscriptions.create!(title: 'Creator Tier', price: 100, status: true)
-subscription2 = customer1.subscriptions.create!(title: 'Mint Tier', price: 200, status: false)
+subscription1 = customer1.subscriptions.create!(title: 'Creator Tier', price: 100, status: 0, frequency: 1)
+subscription2 = customer1.subscriptions.create!(title: 'Mint Tier', price: 200, status: 1, frequency: 0)
 
 tea1 = Tea.create!(title: 'Peach', description: 'Peach tea leaves', temperature: 90, brew_time: 100)
 tea2 = Tea.create!(title: 'Mint', description: 'Mint tea leaves', temperature: 90, brew_time: 100)
